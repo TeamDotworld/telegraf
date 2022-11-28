24db2fdaaae957cac222aea95fe46b1333af731b
cipher <cipher@kali.kali>
2022-11-28 13:25:16 +0530

    Added build script and gitignore

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 archs=(amd64 arm64 386 arm)
-os=(linux windows)
+os=(windows linux)
 
 for os in ${os[@]}
 do
@@ -8,11 +8,13 @@ do
     do
         echo "Building $os-$arch"
         if [ $os = "windows" ]; then
-            env GOOS=$os GOARCH=$arch go build -o telegraf_build/telegraf_$os_$arch.exe -tags "goplugin" -ldflags " -s -w -X main.commit=2101b2ba -X main.branch=dotwo_battery -X main.goos=$os -X main.goarch=$arch -X main.version=1.23.4-2101b2ba" ./cmd/telegraf
-            zip telegraf_build/telegraf_$os_$arch.zip telegraf_build/telegraf_$os_$arch.exe
+            if [ $arch = "386" ] || [ $arch = "amd64" ]; then
+                env GOOS=$os GOARCH=$arch go build -o telegraf_build/telegraf_$arch.exe -tags "goplugin" -ldflags " -s -w -X main.commit=2101b2ba -X main.branch=dotwo_battery -X main.goos=$os -X main.goarch=$arch -X main.version=1.23.4-2101b2ba" ./cmd/telegraf
+                zip telegraf_build/telegraf_$arch.zip telegraf_build/telegraf_$arch.exe
+            fi
         else
-            env GOOS=$os GOARCH=$arch go build -o telegraf_build/telegraf_$os_$arch -tags "goplugin" -ldflags " -s -w -X main.commit=2101b2ba -X main.branch=dotwo_battery -X main.goos=$os -X main.goarch=$arch -X main.version=1.23.4-2101b2ba" ./cmd/telegraf
-            tar -czvf telegraf_build/telegraf_$os_$arch.tar.gz telegraf_build/telegraf_$os_$arch
+            env GOOS=$os GOARCH=$arch go build -o telegraf_build/telegraf_$arch -tags "goplugin" -ldflags " -s -w -X main.commit=2101b2ba -X main.branch=dotwo_battery -X main.goos=$os -X main.goarch=$arch -X main.version=1.23.4-2101b2ba" ./cmd/telegraf
+            tar -czvf telegraf_build/telegraf_$arch.tar.gz telegraf_build/telegraf_$arch
         fi
     done
 done
